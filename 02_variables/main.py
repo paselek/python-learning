f5c7a03140f0806c7bed962aaea5ce5f440bfe2a
Paselekk <162314678+paselek@users.noreply.github.com>
2026-06-01 23:23:14 +0200

    add basic variables

---
+++ 02_variables/main.py
@@ -0,0 +1,17 @@
+# Variable is a container for a value (strin, integer, float, boolean)
+
+#String
+first_name = "Pawel"
+print(f"Hello I'm {first_name}")
+
+#Integer
+age = 25
+print(f"I am {age} years old")
+
+#Float
+height = 1.8
+print(f"My height is {height} m")
+
+#Boolean
+is_graduate = True
+print(f"Am I a graduate? {is_graduate}")
