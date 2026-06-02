1c04955c1c67fd3f3d2dc1221b470a3624953bd3
Paselekk <162314678+paselek@users.noreply.github.com>
2026-06-02 16:35:33 +0200

    add madlibs game

---
+++ 05_madlibsGame/main.py
@@ -0,0 +1,11 @@
+#madlibs game - word game where u create story by filling in blanks with random words
+
+adjective1 = input("Enter an adjective: ") #f.e. "funny", "beautiful", "strange"
+noun1 = input("Enter a noun: ") #f.e. "cat", "car", "house"
+place1 = input("Enter a place: ") #f.e. "park", "school", "store"
+verb1 = input("Enter a verb: ") #f.e. "ran", "jumped", "danced"
+
+
+print(f"Yesterday, I saw a {adjective1} {noun1} in {place1}.")
+print(f"The {noun1} started to {verb1}, so i decided to {verb1} too!")
+print(f"Everyone in {place1} said it was the most {adjective1} thing they had ever seen.")
